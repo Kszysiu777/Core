a5d3e534a30eb6f8a38e6adbf3c0d26a68803563
Krzysztof Bizon <kbizon@c1r1s7.42prague.com>
2024-03-13 17:18:47 +0100

    next line

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,11 +6,41 @@
 /*   By: kbizon <kbizon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 14:21:29 by kbizon            #+#    #+#             */
-/*   Updated: 2024/03/13 17:12:19 by kbizon           ###   ########.fr       */
+/*   Updated: 2024/03/13 17:15:57 by kbizon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*join;
+	unsigned int	i;
+	unsigned int	j;
+
+	join = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!join)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		join[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		join[j++] = s2[i++];
+	join[j] = 0;
+	return (join);
+}
 
 char	*ft_strchr(const char *s, int c)
 {
